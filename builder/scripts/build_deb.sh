cb421146ad5ceb32f1aac585724e1bd0e1e3cfb3
Pix3lPunch <k101@outlook.com>
2023-12-11 20:28:59 +0300

    Scripts fix

--- builder/scripts/build_deb.sh
+++ builder/scripts/build_deb.sh
@@ -16,6 +16,7 @@ PACKAGE_VERSION=$(python3 setup.py --version)
 PACKAGE_LICENSE=$(python3 setup.py --license | tr '[:upper:]' '[:lower:]')
 PACKAGE_EMAIL=$(python3 setup.py --author-email)
 export DEBFULLNAME=$(python3 setup.py --author)
+export $(grep -v '^#' /build/.env | xargs)
 
 #Install python requirements
 python3 -m pip install -r requirements.txt
@@ -25,19 +26,18 @@ dh_make -s -n -c "$PACKAGE_LICENSE" -p "${PACKAGE_NAME}_${PACKAGE_VERSION}" -e "
 
 #Fill debian/control
 PACKAGE_DESCRIPTION=$(python3 setup.py --description)
-PYTHON_VER=3.8
-DEPENDENCIES="python3 (>=$PYTHON_VER), debhelper (>=7), dh-python, python3-setuptools"
+BUILD_DEPENDENCIES="python3 (>=${PYTHON_VER}), debhelper (>=7), dh-python, python3-setuptools"
 
 sed -i "s/Description: .*/Description: $PACKAGE_DESCRIPTION/g" "$SRC_PATH/debian/control"
 sed -i "s/Section: .*/Section: python/g" "$SRC_PATH/debian/control"
-sed -i "s/Build-Depends: .*/Build-Depends: $DEPENDENCIES/g" "$SRC_PATH/debian/control"
+sed -i "s/Build-Depends: .*/Build-Depends: $BUILD_DEPENDENCIES/g" "$SRC_PATH/debian/control"
 sed -i "/Standards-Version:/a\XS-Version: $PYTHON_VER" "$SRC_PATH/debian/control"
 
 #Fill debian/rules
 sed -i "s/\tdh \$@.*/\tdh \$@ --with python3 --buildsystem=pybuild/g" "$SRC_PATH/debian/rules"
 
 #Fill compat
-echo "12" > "$SRC_PATH/debian/compat"
+echo "$COMPAT_LEVEL" > "$SRC_PATH/debian/compat"
 
 #Start build
 dpkg-buildpackage
